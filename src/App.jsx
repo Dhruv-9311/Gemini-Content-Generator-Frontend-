5da17fa2c43934719a950a4d192834032cdd5d1c
Coding_by-dhruv <167305177+Dhruv-9311@users.noreply.github.com>
2025-08-21 14:41:44 +0530

    Adding Frontend Projects

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import CurrencyConverter from "./Components/CurrencyConverter";
+
+function App() {
+  return (
+    <>
+    <div className="min-h-screen bg-gray-100"
+    >
+     <div className="py-8">
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Currency Exchange</h1>
+      <CurrencyConverter />
+      </div> 
+
+    </div>
+    </>
+    
+
+)
+  }
+
+export default App;
